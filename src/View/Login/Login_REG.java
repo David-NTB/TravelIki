3e65a13d25745a36c1208a2243c759b0cc2433e5
Traveliki- <https://github.com/duriskifeb>
2024-06-11 22:52:18 +0700

    Done pembenaran peringatan Already exist acount pada register

--- src/View/Login/Login_REG.java
+++ src/View/Login/Login_REG.java
@@ -12,7 +12,7 @@ public class Login_REG {
 
 	private JFrame frmLoginPanel;
 	private JTextField user;
-	private JTextField passw;
+	private JPasswordField passw;
 	private JComboBox<String> rol;
 	private JButton btnLogin;
 	private JLabel user_lbl;
@@ -52,9 +52,23 @@ public class Login_REG {
 		frmLoginPanel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frmLoginPanel.getContentPane().setLayout(null);
 
+		JLabel lblUsername = new JLabel("Username : ");
+		lblUsername.setFont(new Font("Poppins", Font.BOLD, 16)); // Perbesar teks
+		lblUsername.setBounds(41 + 445, 10, 120, 30); // Sesuaikan bounds agar lebih besar
+		frmLoginPanel.getContentPane().add(lblUsername);
 		
-
 		user = new JTextField();
+		user.setBounds(140 + 445, 10, 250, 30); // Perbesar ukuran dan sesuaikan posisi
+		frmLoginPanel.getContentPane().add(user);
+		user.setColumns(10);
+		
+		user_lbl = new JLabel("Username Already Exist !!!");
+		user_lbl.setForeground(new Color(255, 0, 0));
+		user_lbl.setFont(new Font("Poppins", Font.BOLD, 14)); // Menambahkan gaya bold pada teks
+		user_lbl.setVisible(false);
+		user_lbl.setBounds(140 + 449, 34, 250, 30); // Sesuaikan posisi dan ukuran
+		frmLoginPanel.getContentPane().add(user_lbl);
+		
 		user.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent arg0) {
@@ -75,24 +89,14 @@ public class Login_REG {
 			}
 		});
 
-		JLabel lblUsername = new JLabel("Username : ");
-		lblUsername.setFont(new Font("Poppins", Font.BOLD, 16)); // Perbesar teks
-		lblUsername.setBounds(41 + 445, 10, 120, 30); // Sesuaikan bounds agar lebih besar
-		frmLoginPanel.getContentPane().add(lblUsername);
-
-		user = new JTextField();
-		user.setBounds(140 + 445, 10, 250, 30); // Perbesar ukuran dan sesuaikan posisi
-		frmLoginPanel.getContentPane().add(user);
-		user.setColumns(10);
-
 		JLabel lblPassword = new JLabel("Password : ");
 		lblPassword.setFont(new Font("Poppins", Font.BOLD, 16)); // Perbesar teks
-		lblPassword.setBounds(41 + 445, 48, 120, 30); // Tempatkan di bawah Username
+		lblPassword.setBounds(41 + 445, 55, 120, 30); // Tempatkan di bawah Username
 		frmLoginPanel.getContentPane().add(lblPassword);
 
 		passw = new JPasswordField();
 		passw.setColumns(10);
-		passw.setBounds(140 + 445, 48, 250, 30); // Tempatkan di bawah Username
+		passw.setBounds(140 + 445, 55, 250, 30); // Tempatkan di bawah Username
 		frmLoginPanel.getContentPane().add(passw);
 
 		// Register button
@@ -104,24 +108,17 @@ public class Login_REG {
 					JOptionPane.showMessageDialog(null, "Select Your Role");
 				} else {
 					String un = user.getText();
-					String ps = passw.getText();
+					String ps = new String(passw.getPassword());
 					String role = rol.getSelectedItem().toString();
 					new Login_REG_ctrl(un, ps, role);
 					JOptionPane.showMessageDialog(null, "Registration Successful");
 
 					// Tutup jendela Login_REG
 					frmLoginPanel.dispose();
-
-					// Tampilkan kembali splash screen
-					// try {
-					// LOG_Splash.main(null);
-					// } catch (InterruptedException e) {
-					// e.printStackTrace();
-					// }
 				}
 			}
 		});
-		btnRegister.setBounds(590, 135, 100, 30); // Menyesuaikan dengan posisi dan ukuran tombol "Reset"
+		btnRegister.setBounds(590, 145, 100, 30); // Menyesuaikan dengan posisi dan ukuran tombol "Reset"
 		frmLoginPanel.getContentPane().add(btnRegister);
 
 		// Reset button
@@ -134,51 +131,23 @@ public class Login_REG {
 				rol.setSelectedIndex(0); // Reset combo box to default
 			}
 		});
-		btnReset.setBounds(330 + 415, 135, 100, 30); // Adjusted position and size
+		btnReset.setBounds(330 + 415, 145, 100, 30); // Adjusted position and size
 		frmLoginPanel.getContentPane().add(btnReset);
 
 		// Initialize and set up the JComboBox
 		rol = new JComboBox<>();
 		rol.setModel(
 				new DefaultComboBoxModel<>(new String[] { "Select", "Employee", "Manager", "Admin", "Super_Admin" }));
-		rol.setBounds(140 + 447, 84, 250, 30); // Position below Password
+		rol.setBounds(140 + 447, 95, 250, 30); // Position below Password
 		frmLoginPanel.getContentPane().add(rol); // Add to the content pane
 
 		// Initialize and set up the JLabel for Role
 		JLabel lblRole = new JLabel("Role : ");
 		lblRole.setFont(new Font("Poppins", Font.BOLD, 16)); // Match the font
-		lblRole.setBounds(41 + 447, 84, 120, 30); // Position below Password
+		lblRole.setBounds(41 + 447, 95, 120, 30); // Position below Password
 		frmLoginPanel.getContentPane().add(lblRole); // Add to the content pane
 
-		user_lbl = new JLabel("Username Already Exist !!!");
-		user_lbl.setForeground(new Color(255, 0, 0));
-		user_lbl.setVisible(false);
-		user_lbl.setBounds(140 + 445, 40, 200, 30); // Sesuaikan posisi dan ukuran
-		frmLoginPanel.getContentPane().add(user_lbl);
-
-		user = new JTextField();
-		user.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent arg0) {
-				String usr = user.getText();
-				String ad = Login_Admin.username_exist_admin(usr);
-				String em = Login_Employee.username_exist_emp(usr);
-				String mng = Login_Manager.username_exist_mngr(usr);
-
-				if (ad == null && em == null && mng == null) {
-					user.setBorder(null);
-					user_lbl.setVisible(false);
-					btnLogin.setEnabled(true);
-				} else {
-					user.setBorder(new LineBorder(new Color(255, 0, 0)));
-					user_lbl.setVisible(true);
-					btnLogin.setEnabled(false);
-				}
-			}
-		});
-
-
-		//gambar latar belakang aplikasu?
+		// gambar latar belakang aplikasi
 		JLabel lblImageBottom2 = new JLabel(new ImageIcon(Login_Jfrm.class.getResource("/resource/RevDone1.png")));
 		lblImageBottom2.setBounds(0, 0, 889, 602); // Atur ukuran dan posisi sesuai kebutuhan
 		frmLoginPanel.getContentPane().add(lblImageBottom2);
